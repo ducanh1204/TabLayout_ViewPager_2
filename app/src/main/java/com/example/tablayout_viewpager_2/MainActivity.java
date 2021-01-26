2b163ece1742e0c779887f23ed5938ef61e94777
ducanh1204 <51355767+ducanh1204@users.noreply.github.com>
2021-01-26 10:30:14 +0700

    Initial commit

---
+++ app/src/main/java/com/example/tablayout_viewpager_2/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.tablayout_viewpager_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), ViewPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPagerAdapter.addFrag(new Tab1Fragment(), "Tab1");
+        viewPagerAdapter.addFrag(new Tab2Fragment(), "Tab2");
+        viewPagerAdapter.addFrag(new Tab3Fragment(), "Tab3");
+        viewPagerAdapter.addFrag(new Tab4Fragment(), "Tab4");
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
---
+++ app/src/main/java/com/example/tablayout_viewpager_2/ViewPagerAdapter.java
@@ -0,0 +1,42 @@
+package com.example.tablayout_viewpager_2;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+    private List<Fragment> listFrag = new ArrayList<>();
+    private List<String> listTitle = new ArrayList<>();
+
+    public ViewPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return listFrag.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return listFrag.size();
+    }
+
+    public void addFrag(Fragment fragment, String title) {
+        listFrag.add(fragment);
+        listTitle.add(title);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return listTitle.get(position);
+    }
+}
